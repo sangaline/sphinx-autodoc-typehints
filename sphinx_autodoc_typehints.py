sangaline/sphinx-autodoc-typehints

commit 1599287cbfa48baa2f214249e0bef5520eebce47

feat: Add type hint comment support (#87)
I've made it so that there's as little risk of regression as possible for existing users by shortcutting to `get_type_hints` or `__annotations__` if that approach works. If those return empty mappings, use `typed_ast` with `inspect.getsource` to get type hint comments.

sphinx_autodoc_typehints.py
@@ -1,4 +1,5 @@
 import inspect
+import textwrap
 import typing
 from typing import get_type_hints, TypeVar, Any, AnyStr, Generic, Union
 
@@ -189,6 +190,115 @@ def process_signature(app, what: str, name: str, obj, options, signature, return
     return signature.format_args().replace('\\', '\\\\'), None
 
 
+def get_all_type_hints(obj, name):
+    rv = {}
+
+    try:
+        rv = get_type_hints(obj)
+    except (AttributeError, TypeError):
+        # Introspecting a slot wrapper will raise TypeError
+        pass
+    except NameError as exc:
+        logger.warning('Cannot resolve forward reference in type annotations of "%s": %s',
+                       name, exc)
+        rv = obj.__annotations__
+
+    if rv:
+        return rv
+
+    rv = backfill_type_hints(obj, name)
+
+    try:
+        obj.__annotations__ = rv
+    except AttributeError:
+        return rv
+
+    try:
+        rv = get_type_hints(obj)
+    except (AttributeError, TypeError):
+        pass
+    except NameError as exc:
+        logger.warning('Cannot resolve forward reference in type annotations of "%s": %s',
+                       name, exc)
+        rv = obj.__annotations__
+
+    return rv
+
+
+def backfill_type_hints(obj, name):
+    rv = {}
+
+    try:
+        import typed_ast.ast3 as ast
+    except ImportError:
+        return rv
+
+    def _one_child(module):
+        children = list(ast.iter_child_nodes(module))
+
+        if len(children) != 1:
+            logger.warning(
+                'Did not get exactly one node from AST for "%s", got %s',
+                name,
+                len(children)
+            )
+            return
+
+        return children[0]
+
+    try:
+        obj_ast = ast.parse(textwrap.dedent(inspect.getsource(obj)))
+    except TypeError:
+        return rv
+
+    obj_ast = _one_child(obj_ast)
+    if obj_ast is None:
+        return rv
+
+    try:
+        type_comment = obj_ast.type_comment
+    except AttributeError:
+        return rv
+
+    if not type_comment:
+        return rv
+
+    try:
+        comment_args_str, comment_returns = type_comment.split(' -> ')
+    except ValueError:
+        logger.warning('Unparseable type hint comment for "%s": Expected to contain ` -> `', name)
+        return rv
+
+    if comment_returns:
+        rv['return'] = comment_returns
+
+    if comment_args_str not in ('()', '(...)'):
+        logger.warning(
+            'Only supporting `type: (...) -> rv`-style type hint comments, '
+            'skipping types for "%s"',
+            name
+        )
+        return rv
+
+    try:
+        args = list(ast.iter_child_nodes(obj_ast.args))
+    except AttributeError:
+        logger.warning('No args found on "%s"', name)
+        return rv
+
+    for arg in args:
+        comment = getattr(arg, 'type_comment', None)
+        if not comment:
+            continue
+
+        if not hasattr(arg, 'arg'):
+            continue
+
+        rv[arg.arg] = comment
+
+    return rv
+
+
 def process_docstring(app, what, name, obj, options, lines):
     if isinstance(obj, property):
         obj = obj.fget
@@ -198,15 +308,7 @@ def process_docstring(app, what, name, obj, options, lines):
             obj = getattr(obj, '__init__')
 
         obj = unwrap(obj)
-        try:
-            type_hints = get_type_hints(obj)
-        except (AttributeError, TypeError):
-            # Introspecting a slot wrapper will raise TypeError
-            return
-        except NameError as exc:
-            logger.warning('Cannot resolve forward reference in type annotations of "%s": %s',
-                           name, exc)
-            type_hints = obj.__annotations__
+        type_hints = get_all_type_hints(obj, name)
 
         for argname, annotation in type_hints.items():
             if argname.endswith('_'):

setup.cfg
@@ -30,6 +30,7 @@ install_requires = Sphinx >= 2.1
 test =
     pytest >= 3.1.0
     typing_extensions >= 3.5
+    typed_ast >= 1.4.0
 
 [flake8]
 max-line-length = 99

tests/roots/test-dummy/dummy_module.py
@@ -136,3 +136,44 @@ def function_with_unresolvable_annotation(x: 'a.b.c'):
 
     :param x: foo
     """
+
+
+
+def function_with_typehint_comment(
+    x,  # type: int
+    y   # type: str
+):
+    # type: (...) -> None
+    """
+    Function docstring.
+
+    :param x: foo
+    :param y: bar
+    """
+
+
+class ClassWithTypehints(object):
+    """
+    Class docstring.
+
+    :param x: foo
+    """
+
+    def __init__(
+        self,
+        x  # type: int
+    ):
+        # type: (...) -> None
+        pass
+
+    def foo(
+        self,
+        x  # type: str
+    ):
+        # type: (...) -> int
+        """
+        Method docstring.
+
+        :param x: foo
+        """
+        return 42

tests/test_sphinx_autodoc_typehints.py
@@ -296,4 +296,33 @@ def test_sphinx_output(app, status, warning):
 
            Parameters:
               **x** (*a.b.c*) – foo
+
+        dummy_module.function_with_typehint_comment(x, y)
+
+           Function docstring.
+
+           Parameters:
+              * **x** ("int") – foo
+
+              * **y** ("str") – bar
+
+           Return type:
+              "None"
+
+        class dummy_module.ClassWithTypehints(x)
+
+           Class docstring.
+
+           Parameters:
+              **x** ("int") -- foo
+
+           foo(x)
+
+              Method docstring.
+
+              Parameters:
+                 **x** ("str") -- foo
+
+              Return type:
+                 "int"
         ''').replace('–', '--')

commit e21bc67bec7584ddea3b0f1021901d57ff150c4d

Fixed regression caused by PR #87

sphinx_autodoc_typehints.py
@@ -206,7 +206,7 @@ def get_all_type_hints(obj, name):
 
     try:
         obj.__annotations__ = rv
-    except AttributeError:
+    except (AttributeError, TypeError):
         return rv
 
     try: